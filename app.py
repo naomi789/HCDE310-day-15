516bdd650eb6b72ec285172f3b3b461dbc1ef321
Naomi Johnson <snjohnson789@gmail.com>
2024-11-19 07:36:49 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+@app.route("/")
+def index():
+    fruits = [{"name": "apples", "quantity": 3},
+            {"name": "oranges", "quantity": 2},
+            {"name": "strawberries", "quantity": 6}]
+    return render_template("index.html", fruits = fruits)
