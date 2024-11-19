4ab9b512f5d4bb2d7eeb3bb3e8c0382eda7bd0cc
Naomi Johnson <snjohnson789@gmail.com>
2024-11-19 08:13:20 -0800

    fixing syntax

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    fruits = [{"name": "apples", "quantity": 3},
+    fruits = [
+        {"name": "apples", "quantity": 3},
             {"name": "oranges", "quantity": 2},
-            {"name": "strawberries", "quantity": 6}]
-    return render_template("index.html", fruits = fruits)
+            {"name": "strawberries", "quantity": 6}
+    ]
+    return render_template("index.html", fruits=fruits)
